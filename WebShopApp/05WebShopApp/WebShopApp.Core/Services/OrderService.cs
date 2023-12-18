557de142288afd291db31ffb6279d95862297a42
Your Name <you@example.com>
2023-12-18 11:39:36 +0200

    5th order

---
+++ WebShopApp/05WebShopApp/WebShopApp.Core/Services/OrderService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Server.IIS.Core;
+
+using WebShopApp.Core.Contracts;
+using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Core.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IProductService _productService;
+
+        public OrderService(ApplicationDbContext context, IProductService productService)
+        {
+            _context = context;
+            _productService = productService;
+        }
+        public bool Create(int productId, string userId, int quantity)
+        {
+            var product = this._context.Products.SingleOrDefault(x => x.Id == productId);
+            if (product==null)
+            {
+                return false;
+            }
+
+            Order item = new Order
+            {
+                OrderDate = DateTime.Now,
+                ProductId = productId,
+                UserId = userId,
+                Quantity = quantity,
+                Price = product.Price,
+                Discount = product.Discount
+            };
+
+            product.Quantity -= quantity;
+            this._context.Products.Update(product);
+            this._context.Orders.Add(item);
+
+            return _context.SaveChanges()!=0;
+        }
+
+        public List<Order> GetOrders()
+        {
+            return _context.Orders.OrderByDescending(x=>x.OrderDate).ToList(); 
+        }
+
+        public Order GetOrdersById(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetOrdersByUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBy(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int orderId, int productId, string userId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebShopApp/05WebShopApp/WebShopApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtention.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using WebShopApp.Infrastructure.Data.Domain;
+
+namespace WebShopApp.Infrastructure.Data.Infrastructure
+{
+    public static class ApplicationBuilderExtention
+    {
+        public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var services = serviceScope.ServiceProvider;
+
+            await RoleSeeder(services);
+            await SeedAdimistrator(services);
+
+
+            var dataCategory = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            SeedCategories(dataCategory);
+
+
+            var dataBrand = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            SeedBrands(dataBrand);
+            return app;
+        }
+
+
+        public static void SeedCategories(ApplicationDbContext dataCategory)
+        {
+            if (dataCategory.Categories.Any())
+            {
+                return;
+            }
+            dataCategory.Categories.AddRange(new[]
+            {
+                new Category {CategoryName = "Lapton"},
+                new Category {CategoryName = "Computer"},
+                new Category {CategoryName = "Monitor"},
+                new Category {CategoryName = "Accessory"},
+                new Category {CategoryName = "TV"},
+                new Category {CategoryName = "Mobile phone"},
+                new Category {CategoryName = "Smart watch"},
+            });
+            dataCategory.SaveChanges();
+        }
+
+
+        public static void SeedBrands(ApplicationDbContext dataBrand)
+        {
+            if (dataBrand.Brands.Any())
+            {
+                return;
+            }
+            dataBrand.Brands.AddRange(new[]
+            {
+                new Brand {BrandName = "Acer"},
+                new Brand {BrandName = "Asus"},
+                new Brand {BrandName = "Apple"},
+                new Brand {BrandName = "Dell"},
+                new Brand {BrandName = "HP"},
+                new Brand {BrandName = "Huawei"},
+                new Brand {BrandName = "Lenovo"},
+                new Brand {BrandName = "Samsung"},
+            });
+            dataBrand.SaveChanges();
+        }
+
+
+        private static async Task RoleSeeder(IServiceProvider servicepProvider)
+        {
+            var roleManager = servicepProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Administrator", "Client" };
+            IdentityResult roleResult;
+            foreach (var role in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+
+        private static async Task SeedAdimistrator(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            if (await userManager.FindByNameAsync("admin") == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.FirstName = "admin";
+                user.LastName = "admin";
+                user.UserName = "admin";
+                user.Email = "admin@admin.com";
+                user.Address = "admin";
+                user.PhoneNumber = "0895519837";
+
+                var result = await userManager.CreateAsync(user, "123123");
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+
+            }
+        }
+    }
+}
